c6e214792a923a6522c9d93ecb5a189dcecea707
schuang <m15876657435@163.com>
2019-11-28 01:57:54 +0800

    Add vector printer, bubble sort routine, brace checker tool

--- README.md
+++ README.md
@@ -6,6 +6,15 @@ Implementations of **data structures** and **algorithms** using **C++** by *Junk
 ### pTime
 A simple tool using `std::chrono` to measure how long a program runs. And you can set multiple checkpoints conveniencely in a program.
 
+### myPrint
+Print vector in various mode to given ostream object.
+
+### mySwap
+Swap two elements in a vector.
+
+### myBubbleSort
+Bubble sort implementation.
+
 
 ### Selection Problem
 
@@ -13,3 +22,7 @@ A simple tool using `std::chrono` to measure how long a program runs. And you ca
 
 **Output:** 	the k-th largest number  of input 
 Using `std::priority_queue`.
+
+### myBraceChecker
+A simple tools to check brace balance of a file. 
+Using `std::stack`.
--- myDSA.h
+++ myDSA.h
@@ -1,6 +1,7 @@
 /**
  * This is the header of myDSA.cpp
  * myDSA includes implementations of various data structures and algorithms
+ * all of the functions are named with a "my-" prefix for distinguishing.
  * @author Junkang_Huang huangjk8@mail2.sysu.edu.cn
 **/
 
@@ -10,9 +11,11 @@
 # include <vector>
 # include <string>
 # include <queue>
+# include <stack>
 # include <ctime>
 # include <chrono>
 # include <iostream>
+# include <fstream>
 
 using std::cin;
 using std::cout;
@@ -22,7 +25,9 @@ using std::vector;
 using std::string;
 using std::priority_queue;
 
-
+//
+// -------------------- pTime --------------------
+//
 // 
 //  A routine to count the time cost by a program in milliseconds.
 //  Using std::chrono library and std::chrono::steady_clock as clock epoch.
@@ -39,10 +44,10 @@ class pTime{
     using time_point_t = std::chrono::time_point<std::chrono::steady_clock, std::chrono::milliseconds>;
 private:
     // Member variables
-    time_point_t start;
-    time_point_t end;
-    vector<time_point_t> points;
-    bool counting = false;
+    time_point_t start; // Start of counting
+    time_point_t end;   // End of counting
+    vector<time_point_t> points;    // Checkpoints during counting
+    bool counting = false;  // Whether pTime is still running
 public:
     // Start couting
     void pStart(){
@@ -80,26 +85,153 @@ public:
             cerr << "pTime is still counting !" << endl;
             return;
         }
+        // Display checkpoints
         for (auto i = 0; i < points.size(); ++i)
             cout << "# " << i+1 << "    " << (points[i]-start).count() << " ms" << endl;
+        // Display total time
         cout << endl << "Running time:     " << (end - start).count() << " ms" << endl;
         return;
     }
 };
 
+//
+// -------------------- Fundamental --------------------
+//
+
+//
+// Print:
+//  Print a vector
+//    v                          - the vector need to print
+//    os                         - ostream object where the vector to be print to
+//    mode                       - "Normal" : print in one line
+//                                 "Vertical" : print one item a line
+//
+template <typename T>
+void myPrint(const vector<T> &v, std::ostream &os = std::cout, string mode = "Normal") {
+    if ( mode == "Normal" ) {
+        // "Normal" mode: print in the form as : [ a1, a2, a3, ... ]
+        os << "[ ";
+        for (auto i = 0; i < v.size()-1; ++i)
+            os << v[i] << ", ";
+        os << v.back() << " ]" << endl;
+    } else if ( mode == "Vertical" ) {
+        // "Vertical" mode: print each item at a line
+        for (auto i = 0; i < v.size()-1; ++i)
+            os << v[i] << endl;
+        os << v.back() << endl;
+    }
+    return;
+}
+
+//
+// Swap:
+//  Swap two elements of a vector
+//
+template <typename T>
+void mySwap (vector<T> &v, int a, int b) {
+    if ( a == b ) return;
+    if ( a > v.size() || b > v.size() || a < 0 || b < 0 ) {
+        cerr << "Error: Invalid index." << endl;
+        return;
+    }
+    std::swap(v[a], v[b]);
+    return;
+}
+
+
+
+
+//
+// -------------------- Sort --------------------
+//
+
+//
+// Bubble sort:
+//
+template <typename T>
+void myBubbleSort(vector<T> &v) {
+    while (true){
+        int swapped = false;
+        for (auto i = 0; i < v.size() - 1; ++i){
+            if (v[i] > v[i+1]){
+                std::swap(v[i], v[i+1]);
+                swapped = true;
+            }
+        }
+        if ( !swapped ) break;
+    }
+    return;
+}
+
+
+//
+// -------------------- Problems & Other Tools --------------------
+//
 
 //
 //  Selection Problem solution 1:
 //  Using std::priority_queue to record all the numbers, and pop() for k times
 //
 template <typename T>
-T selectionProblem1(const vector<T> &array, int k){
+T mySelectionProblem1(const vector<T> &array, int k){
     priority_queue<T> pq;
     for (auto item : array)
         pq.push(item);
     for (auto i = 0; i < k - 1; ++i)
         pq.pop();
     return pq.top();
+}
+
+//
+//  Brace checker:
+//  Check a file string object (e.g. extracted from a file) whether the braces are balanced ("(", "[", "{"). 
+//  Using a std::stack data structure
+//
+void myBraceChecker(const string &fileName) {
+    std::ifstream input(fileName);
+    // To record current position
+    unsigned line = 1, column = 1;
+    // Indicate whether there is an brace error
+    bool error = false;
+    // Implement with std::stack data structure
+    std::stack<char> braceStack;
+    char c;
+    while (input.get(c)){
+        if ( c == ')' ){
+            if (braceStack.empty() || braceStack.top() != '('){
+                error = true;
+                break;
+            }
+            else braceStack.pop();
+        } else if ( c == ']' ){
+            if (braceStack.empty() || braceStack.top() != '['){
+                error = true;
+                break;
+            }
+            else braceStack.pop();
+        } else if ( c == '}' ){
+            if (braceStack.empty() || braceStack.top() != '{'){
+                error = true;
+                break;
+            }
+            else braceStack.pop();
+        } else {
+            // Update line and column
+            if ( c == '\n' ) {
+                ++line;
+                column = 0;
+            } else if ( c == '(' || c == '[' || c == '{' )
+                braceStack.push(c);
+            ++column;
+        }        
+    }
+    // Print the result
+    if ( error ){
+        cout << "Cannot find matching brace at:" << endl;
+        cout << "Line: " << line << ",   Column: " << column << endl;
+        return;
+    } else 
+        cout << "Perfect!" << endl;
 }
 
 # endif
